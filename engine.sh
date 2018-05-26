f146462fea213718f54c162c720883d6d255da0e
telekitamas <32730649+telekitamas@users.noreply.github.com>
2018-05-26 16:57:15 +0200

    Update engine.sh

--- engine.sh
+++ engine.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#Docker engine-hez szükséges csomagok telepítése
 username='ubuntu'
 sudo apt-get update
 sudo apt-get install nano git apt-transport-https -y
@@ -7,13 +8,21 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
 apt-cache policy docker-ce
+
+#Docker engine telepítése
 sudo apt-get install -y docker-ce
 sudo systemctl status docker | grep docker.service
+
+#Sudo jog beállítása
 sudo usermod -aG docker $username && echo 'OK'
+
+#Szükséges mappák létrehozása
 cd ~
 mkdir Docker
 cd Docker
 mkdir web
+
+#Dockerfile létrehozása, szerkesztése
 touch Dockerfile
 cat <<EOF> Dockerfile
 FROM nginx
@@ -22,11 +31,17 @@ RUN cd /usr/share/nginx/html/ && rm *
 COPY web /usr/share/nginx/html
 EXPOSE 80 443
 CMD ["nginx", "-g", "daemon off;"]
-EOF
+EOF #End Of File
+
+#HTML file létrehozása, szerkesztése
 touch web/index.html
 cat <<EOF> web/index.html
+
+#Megjelenítendő szöveg
 Sikeres vizsga!
-EOF
+EOF #End Of File
+
+#Docker image készítése, futtatása
 cd ~
 cd Docker
 sudo docker build -t nginx .
